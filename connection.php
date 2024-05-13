25f0f15813ce1dc9ba2617a940a6a859ea5a4cff
Rajaram Yadav <yrajaram112@gmail.com>
2024-05-12 23:13:37 -0400

    Deployment changes

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $database= new mysqli("localhost","root","","edoc");
+    $database= new mysqli("localhost","praveen.m","FJYWWJDU","praveenmanandhar_");
     if ($database->connect_error){
         die("Connection failed:  ".$database->connect_error);
     }
